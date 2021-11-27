2adc788cab383ad0824a9080d0cc3c4a22daf45d
Swati-1229 <77539324+Swati-1229@users.noreply.github.com>
2021-11-28 00:46:33 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,36 +1,3 @@
-#Q1.you need to take input as number from the user and find out the greatest common divisor of those two number and save it in a C drive
-# with the file name as g
-def gcd(a, b):
-    if b == 0:
-       return a
-    else:
-        return gcd(b,a%b)
-a=int(input("enter the first number"))
-b=int(input("enter the second number"))
-print(gcd(a,b))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Q4.write a python program to generate 26 text files named A.txt B.txt and so on upto Z.txt.
 
@@ -39,9 +6,3 @@ alphabet=string.ascii_uppercase
 for letter in alphabet:
  with open(letter+".txt",'w') as  file:
   file.write(letter)
-
-
-
-
-
-
